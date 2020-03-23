fb18c982c3f9e6c6987bf90f2e8dffddbdfd63ce
KonstantinShirmanov <45903009+KonstantinShirmanov@users.noreply.github.com>
2020-03-23 13:05:33 +0300

    commit

---
+++ script.js
@@ -0,0 +1,31 @@
+function access (){
+			while (prompt ('Введите пароль для доступа к странице:') !='11111'){
+				if(confirm ('Пароль неверный. Повторить ввод?')==false){
+						window.location.href ='error.html';
+						break;
+				}
+			}
+		}
+
+function newForm(){
+
+			var x, y;
+			var win; 
+
+			x = (window.outerWidth - 300) / 2 + window.screenX;
+			y = (window.outerHeight - 200) / 2 + window.screenY;
+
+			win = window.open("", "w1", "height = 200, width = 300 , left ="+ x +" ,top =" +y);
+			win.document.write("<h2>Заголовок текста</h2>");
+			win.document.write("<p>javascript — это круто!</p>");
+
+			win.document.write('<p align = "center"><a href = "https://www.dstu.education/"'+' target = "_blank">ДонГТУ</a>');
+
+			win.document.write('<p align = "center"><input type="button" onclick="opener.exit(opener.win)" name="closeButton" value="Закрыть"></input></p>');
+
+		}
+
+function exit (param){
+			param.alert('Внимание окно будет закрыто!');
+			param.close();
+		}
